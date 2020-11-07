813c621b68aadfd658f77cd2b85c2089ac993d2a
Tyrone Moore <mooret025@gmail.com>
2020-11-06 16:21:14 -0800

    Added bank test class

---
+++ test/com/game/monopoly/BankTest.java
@@ -0,0 +1,19 @@
+package com.game.monopoly;
+
+import junit.framework.TestCase;
+
+public class BankTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testPayForProperty() {
+    }
+
+    public void testPayRent() {
+    }
+
+    public void testPayIntoFreeParking() {
+    }
+}
