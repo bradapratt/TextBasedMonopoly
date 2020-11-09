2f41a00f6beeac3ea1663b102959786943f51548
Chris P <indianachamp@gmail.com>
2020-11-09 09:53:35 -0800

    Player changes 11/9/2020

--- src/com/game/monopoly/Player.java
+++ src/com/game/monopoly/Player.java
@@ -1,29 +1,38 @@
 package com.game.monopoly;
 
+import java.util.Collection;
 import java.util.List;
 
-class Player {
+class Player implements Comparable<Player> {
+    private  final int FIRST_LOCATION = 0;
+    private  final int LAST_LOCATION = 37;
     private int wallet;
     private String name;
     private List<Property> properties;
     private Piece gamePiece;
     private int location;
 
+
     /**
      * Constructor: initial values, wallet = 1500, properties = {}, location = 0
      * @param name
      * @param gamePiece
      */
-    public Player(String name, Piece gamePiece) {
+    public Player(String name, Piece gamePiece, int wallet) {
         this.name = name;
         this.gamePiece = gamePiece;
+        setWallet(1500);
     }
 
     /**
      * Get dice roll from game, move spaces (update location), return new location to game.
      */
-    public int takeTurn(int diceRoll){
-        return 0;
+    public int movePlayer(int diceRoll){
+        setLocation(getLocation() + diceRoll);
+        if (getLocation() > LAST_LOCATION){
+            setLocation(getLocation() - LAST_LOCATION - 1);
+        }
+        return getLocation();
     }
 
     /**
@@ -31,6 +40,7 @@ class Player {
      * @param copyOfNewProperty
      */
     private void addProperty(Property copyOfNewProperty){
+       properties.add(copyOfNewProperty);
     }
 
     /**
@@ -38,6 +48,7 @@ class Player {
      * @param propertyName
      */
     private void removeProperty(String propertyName){
+        properties.remove(propertyName);
     }
 
     /**
@@ -87,5 +98,8 @@ class Player {
 
     private void setLocation(int location) {
         this.location = location;
+    }
+    public int compareTo(Player other) {
+        return this.getWallet() - other.getWallet();
     }
 }
